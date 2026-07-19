1606ee90a12373f11c2bd9f8a972edd9e007acc5
gbrhomidi <gbrhomidi@gmail.com>
2026-07-19 16:29:46 +0300

    Create PreviewData.kt

---
+++ app/src/main/java/com/aistudio/dieselstationsms/kxmpzq/ui/preview/PreviewData.kt
@@ -0,0 +1,16 @@
+object PreviewData {
+
+    val dashboardState =
+        DashboardUiState(
+
+            totalSales = 125000,
+
+            totalCustomers = 350,
+
+            totalProducts = 180,
+
+            totalStations = 5
+
+        )
+
+}
